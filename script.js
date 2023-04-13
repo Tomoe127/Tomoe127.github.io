07ffb2e4f78dc8d78040df77abfb94b8854b26e9
Tomoe <eltanin.pe@gmail.com>
2023-04-13 02:07:36 -0500

    funcion carrusel agregado

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// --------------------------------------Modo Dark------------------------------------------
+
 let toggle = document.querySelector(".switch");
 let body = document.body;      
 
@@ -14,4 +16,30 @@ if(valor == "true"){
 } else{
   body.classList.remove("dark")
 }
+
+//------------------------------------------Carrusel---------------------------------------------
+
+var c = 0;
+
+function carrusel(){
+  document.querySelector("#portada").style.opacity = "0";
+  document.querySelector("#portada").style.transition = "opacity 0.5s ease";
+
+  setTimeout(cambio, 500);
+}
+
+function cambio(){
+  c++;
+  if(c > 6){
+    c = 1;
+  }
+  document.querySelector("#portada").style.backgroundImage = `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(../imagenes/portada${c}.jpg)`;
+  document.querySelector("#portada").style.opacity = "1";
+  document.querySelector("#portada").style.transition = "opacity 1s ease";
+
+  setTimeout(carrusel, 2000);
+}
+
+carrusel();
+
 
